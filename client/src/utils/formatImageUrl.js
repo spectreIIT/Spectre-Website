b143a50dda82905d98b1bb7c3e3e24b037c7bef1
spectreIIT <spectre@iitbhilai.ac.in>
2026-05-31 00:08:40 +0530

    Link fix

--- client/src/utils/formatImageUrl.js
+++ client/src/utils/formatImageUrl.js
@@ -4,17 +4,17 @@ export const formatImageUrl = (url) => {
     // 1. https://drive.google.com/file/d/FILE_ID/view...
     const driveFileRegex = /drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)/;
     const match1 = url.match(driveFileRegex);
-    if (match1 && match1[1]) return `https://drive.google.com/thumbnail?id=${match1[1]}&sz=w1000`;
+    if (match1 && match1[1]) return `https://lh3.googleusercontent.com/d/${match1[1]}`;
     
     // 2. https://drive.google.com/open?id=FILE_ID
     const driveOpenRegex = /drive\.google\.com\/open\?id=([a-zA-Z0-9_-]+)/;
     const match2 = url.match(driveOpenRegex);
-    if (match2 && match2[1]) return `https://drive.google.com/thumbnail?id=${match2[1]}&sz=w1000`;
+    if (match2 && match2[1]) return `https://lh3.googleusercontent.com/d/${match2[1]}`;
     
     // 3. https://drive.google.com/uc?id=FILE_ID
-    const driveUcRegex = /drive\.google\.com\/uc\?id=([a-zA-Z0-9_-]+)/;
+    const driveUcRegex = /drive\.google\.com\/uc\?.*?id=([a-zA-Z0-9_-]+)/;
     const match3 = url.match(driveUcRegex);
-    if (match3 && match3[1]) return `https://drive.google.com/thumbnail?id=${match3[1]}&sz=w1000`;
+    if (match3 && match3[1]) return `https://lh3.googleusercontent.com/d/${match3[1]}`;
   } catch (e) {
     console.error('Error formatting image URL', e);
   }
